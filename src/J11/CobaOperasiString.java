043ebd8093384d471d99bf6d934a38ca609a850d
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-08-13 08:22:07 +0700

    Isi1

---
+++ src/J11/CobaOperasiString.java
@@ -0,0 +1,21 @@
+package J11;
+import java.util.Scanner;
+public class CobaOperasiString {
+    public static void main(String[] args){
+        
+        String identitas = "Laveda Nafis Arshanda / X RPL 3 / 19";
+        System.out.println("Identitas : "+ identitas);
+    
+    
+    String x = "Operasi";
+    System.out.println("Isi variabel x : "+ x);
+    System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+     
+    System.out.println("x adalah kosong : " + x.isEmpty());
+        
+    String y = "";
+    System.out.println("isi variabel y : "+ y);
+    System.out.println("y adalah kososng : " + y.isEmpty());
+    }
+}
+
