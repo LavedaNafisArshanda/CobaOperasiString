23470864594d388617ca167519c38d675d1dca45
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-08-14 12:28:26 +0700

    InitialCommit

--- src/J11/CobaOperasiString.java
+++ src/J11/CobaOperasiString.java
@@ -1,21 +1,9 @@
 package J11;
-import java.util.Scanner;
+import java.util.Locale;
 public class CobaOperasiString {
     public static void main(String[] args){
         
         String identitas = "Laveda Nafis Arshanda / X RPL 3 / 19";
         System.out.println("Identitas : "+ identitas);
-    
-    
-    String x = "Operasi";
-    System.out.println("Isi variabel x : "+ x);
-    System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
-     
-    System.out.println("x adalah kosong : " + x.isEmpty());
-        
-    String y = "";
-    System.out.println("isi variabel y : "+ y);
-    System.out.println("y adalah kososng : " + y.isEmpty());
     }
 }
-
