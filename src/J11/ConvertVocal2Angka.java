7e5a54bd8d06626a31e83d94b8fb8813b7987990
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-08-15 09:37:18 +0700

    Initial Commit

---
+++ src/J11/ConvertVocal2Angka.java
@@ -0,0 +1,10 @@
+package J11;
+import java.util.Scanner;
+public class ConvertVocal2Angka {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nConvert Kalimat Alay Angka (vokal ke Angka)\n");}
+public static void main(String[] args){
+    String identitas = "Laveda Nafis Arshanda / X RPL 3 / 19";
+    tampilJudul(identitas);
