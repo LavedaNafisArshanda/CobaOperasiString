c73671061be5f407c285207470e4746e87c36d49
WINDOWS 10 <WINDOWS 10@DESKTOP-BPSCJHN>
2018-08-15 09:58:29 +0700

    Tambah Tampil Per Kata 2

--- src/J11/ConvertVocal2Angka.java
+++ src/J11/ConvertVocal2Angka.java
@@ -26,6 +26,15 @@ public class ConvertVocal2Angka {
        
         } return kalimat;
     }
+        private static void tampilPerKata(String kalimat, String convert) {
+            String[] arrKal = kalimat.split(" ");
+            String[] arrCon = convert.split(" ");
+            
+            for (int i = 0; i < arrCon.length; i++) {
+                System.out.println(arrKal[i]+" => "+arrCon[i]);
+            }
+        }  
+
 
    
     public static void main(String[] args){
@@ -33,5 +42,6 @@ public class ConvertVocal2Angka {
     tampilJudul(identitas);
     String kalimat = tampilInput();
     String convert = vocal2Angka(kalimat);
+    tampilPerKata(kalimat, convert);
  }
 }
